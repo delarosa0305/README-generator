2dfb9890aeb402dbaa488a560dd5ef0ef1723130
delarosa0305 <delarosa2381i@gmail.com>
2022-06-12 12:17:41 -0400

    making readme template

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 // TODO: Include packages needed for this application
-const = fs = require('fs')
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./src/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    return inquir.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your Project?'
+        },
+        {
+            type: 'input',
+            name: 'desction',
+            message: 'What is youir Description?'
+        },
+        {
+            type: '',
+            name: '',
+            message: ''
+        },
+        {
+            type: '',
+            name: '',
+            message: ''
+        },
+        {
+            type: '',
+            name: '',
+            message: ''
+        },
+        {
+            type: '',
+            name: '',
+            message: ''
+        },
+        {
+            type: '',
+            name: '',
+            message: ''
+        },
+        {
+            type: '',
+            name: '',
+            message: ''
+        },
+    ])
 }
 
 // TODO: Create a function to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,54 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  # Table of Contents
+  <nav>
+    <ul>
+      <li>
+        <a href="#description">Description</a>
+      </li>
+      <li>
+        <a href="#installation">Installation</a>
+      </li>
+      <li>
+        <a href="#usage">Usage</a>
+      </li>
+      <li>
+        <a href="#license">License</a>
+      </li>
+      <li>
+        <a href="#contributing">Contributing</a>
+      </li>
+      <li>
+        <a href="#tests">Tests</a>
+      </li>
+      <li>
+        <a href="#questions">Questions</a>
+      </li>
+    </ul>
+  </nav>
+
+  ## <h1 id="description">Description</h1>
+
+
+  ## <h1 id="installation">Installation</h1>
+
+
+  ## <h1 id="usage">Usage</h1>
+
+
+  ## <h1 id="license">License</h1>
+
+
+  ## <h1 id="contributing">Contributing</h1>
+
+
+  ## <h1 id="tests">Tests</h1>
+
+
+  ## <h1 id="questions">Questions</h1>
 
 `;
 }
